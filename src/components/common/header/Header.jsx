ff5afcc6c7eaf6fcfebe422205cce2e845d8c679
ykotesh <ykoteswararao994@gmail.com>
2025-11-26 16:12:48 +0530

    add Popup

--- src/components/common/header/Header.jsx
+++ src/components/common/header/Header.jsx
@@ -1,14 +1,19 @@
-import React, { useState } from "react"
-import { Link } from "react-router-dom"
-import Head from "./Head"
-import "./header.css"
+import React, { useState } from "react";
+import { Link } from "react-router-dom";
+import Head from "./Head";
+import Popup from "../../home/popup";
+import "./header.css";
 
 const Header = () => {
-  const [click, setClick] = useState(false)
+  const [click, setClick] = useState(false);
+  const [openPopup, setOpenPopup] = useState(false);
 
   return (
     <>
       <Head />
+      {openPopup && (
+        <Popup open={openPopup} onClose={() => setOpenPopup(false)}/>
+             )}             
       <header>
         <nav className='flexSB'>
           <ul className={click ? "mobile-nav" : "flexSB "} onClick={() => setClick(false)}>
@@ -34,16 +39,23 @@ const Header = () => {
               <Link to='/contact'>Contact</Link>
             </li>
           </ul>
-          <div className='start'>
-            <div className='button'>GET CERTIFICATE</div>
+          <div className="start">
+            {/* ---------- OPEN POPUP ON CLICK ---------- */}
+            <div
+              className="button"
+              onClick={() => setOpenPopup(true)} // <-- this opens popup
+            >
+              GET CERTIFICATE
+            </div>
           </div>
-          <button className='toggle' onClick={() => setClick(!click)}>
-            {click ? <i className='fa fa-times'> </i> : <i className='fa fa-bars'></i>}
+
+          <button className="toggle" onClick={() => setClick(!click)}>
+            {click ? <i className="fa fa-times"></i> : <i className="fa fa-bars"></i>}
           </button>
         </nav>
       </header>
     </>
-  )
-}
+  );
+};
 
-export default Header
+export default Header;
