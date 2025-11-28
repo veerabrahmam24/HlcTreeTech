b1adff5d21b42aca0d72c9c9bf2b60a098729e96
mkcompanyin1998-svg <mkcompany.in1998@gmail.com>
2025-11-29 00:10:12 +0530

    changes icones

--- src/components/common/header/Head.jsx
+++ src/components/common/header/Head.jsx
@@ -1,25 +1,52 @@
-import React from "react"
+import React from "react";
 
 const Head = () => {
   return (
     <>
-      <section className='head'>
-        <div className='container flexSB'>
-          <div className='logo'>
-            <h1>ACADEMIA</h1>
-            <span>Learn Today. Build Your Tomorrow. With Our Support.Learn Today. Build Your Tomorrow. With Our Support.</span>
+      <section className="head" style={{ paddingLeft: "-10px" }}>
+        <div className="container flexSB">
+          <div
+            className="logo"
+            style={{ display: "flex", alignItems: "center", gap: "20px" }}
+          >
+            <img
+              src="/images/vhlc999.png"
+              alt="Logo"
+              style={{
+                width: "150px",
+                height: "90px",
+                objectFit: "cover",
+                marginLeft: "-85px",
+                borderRadius: "10px"
+               
+              }}
+            />
+
+            <div>
+              <h1>HLC Tree Technologies</h1>
+              <span>Learn Today.Build Your Tomorrow.With Our Support.</span>
+
+            </div>
           </div>
 
-          <div className='social'>
+          <div className="social">
+            <a href="https://www.facebook.com" target="_blank" rel="noopener noreferrer">
             <i className='fab fa-facebook-f icon'></i>
-            <i className='fab fa-instagram icon'></i>
+             </a>
+            <a href="https://www.twitter.com" target="_blank" rel="noopener noreferrer">
             <i className='fab fa-twitter icon'></i>
-            <i className='fab fa-youtube icon'></i>
+            </a>
+           <a href="https://www.instagram.com" target="_blank" rel="noopener noreferrer">
+            <i className='fab fa-instagram icon'></i>
+            </a>
+            <a href="https://youtu.be/5JoPeWHkKJg?si=yKn8_hKV9w0ge-EF" target="_blank" rel="noopener noreferrer">
+            <i className="fab fa-youtube icon"></i>
+            </a>
           </div>
         </div>
       </section>
     </>
-  )
-}
+  );
+};
 
-export default Head
+export default Head;
--- src/components/home/hero/Hero.jsx
+++ src/components/home/hero/Hero.jsx
@@ -1,4 +1,5 @@
 import React from "react"
+import { Link } from "react-router-dom"
 import Heading from "../../common/heading/Heading"
 import "./Hero.css" 
 
@@ -8,16 +9,19 @@ const Hero = () => {
       <section className='hero'>
         <div className='container'>
           <div className='row'>
-            <Heading subtitle='WELCOME TO ACADEMIA' title='Best Online Education Expertise' />
+            <Heading subtitle='WELCOME TO HLC Tree Technologies' title='Best Online Education Expertise' />
             <p>Far far away, behind the word mountains, far from the countries Vokalia and Consonantia, there live the blind texts.</p>
             <div className='button'>
+             <a href="/courses">
               <button className='primary-btn'>
                 GET STARTED NOW <i className='fa fa-long-arrow-alt-right'></i>
               </button>
+               </a> 
+             {/* <a href="/courses">
              <button>
                 VIEW COURSE <i className='fa fa-long-arrow-alt-right'></i>
               </button>
-              
+             </a>  */}
             </div>
           </div>
         </div>
